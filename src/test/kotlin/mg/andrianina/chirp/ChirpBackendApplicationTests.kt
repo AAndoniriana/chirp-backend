f3b0323d9c1c62a32ee3cc1d67113cfcbb141255
Andrianaivo Andoniriana Andrianina <andrian.andrianaivo@gmail.com>
2025-09-15 18:37:09 +0300

    Init the project

---
+++ src/test/kotlin/mg/andrianina/chirp/ChirpBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package mg.andrianina.chirp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChirpBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
