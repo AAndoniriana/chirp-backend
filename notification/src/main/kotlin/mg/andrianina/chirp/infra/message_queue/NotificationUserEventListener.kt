c80c3984bf3a3809ef5cc5ccc5b55fb9a69ce5cb
Andrianaivo Andoniriana Andrianina <andrian.andrianaivo@gmail.com>
2025-11-21 09:24:02 +0300

    Configure mail notification

--- notification/src/main/kotlin/mg/andrianina/chirp/infra/message_queue/NotificationUserEventListener.kt
+++ notification/src/main/kotlin/mg/andrianina/chirp/infra/message_queue/NotificationUserEventListener.kt
@@ -1,22 +1,40 @@
 package mg.andrianina.chirp.infra.message_queue
 
 import mg.andrianina.chirp.domain.events.user.UserEvent
+import mg.andrianina.chirp.service.EmailService
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
+import java.time.Duration
 
 @Component
-class NotificationUserEventListener {
+class NotificationUserEventListener(private val emailService: EmailService) {
     @RabbitListener(queues = [MessageQueue.NOTIFICATION_USER_EVENT])
     fun handleUserEvent(event: UserEvent) {
         when (event) {
             is UserEvent.Created -> {
-                println("User created")
+                emailService.sendVerificationEmail(
+                    email = event.email,
+                    username = event.username,
+                    userId = event.userId,
+                    token = event.verificationToken
+                )
             }
             is UserEvent.RequestResendVerification -> {
-                println("Request resend verification")
+                emailService.sendVerificationEmail(
+                    email = event.email,
+                    username = event.username,
+                    userId = event.userId,
+                    token = event.verificationToken
+                )
             }
             is UserEvent.RequestResetPassword -> {
-                println("Request reset password")
+                emailService.sendPasswordResetEmail(
+                    email = event.email,
+                    username = event.username,
+                    userId = event.userId,
+                    token = event.verificationToken,
+                    expiresIn = Duration.ofMinutes(event.expiresInMinutes)
+                )
             }
             is UserEvent.Verified -> {
                 println("User verified")
---
+++ notification/src/main/kotlin/mg/andrianina/chirp/service/EmailService.kt
@@ -0,0 +1,101 @@
+package mg.andrianina.chirp.service
+
+import mg.andrianina.chirp.domain.type.UserId
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.mail.MailException
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
+import org.springframework.stereotype.Service
+import org.springframework.web.util.UriComponentsBuilder
+import java.time.Duration
+
+@Service
+class EmailService(
+    private val javaMailSender: JavaMailSender,
+    private val emailTemplateService: EmailTemplateService,
+    @param:Value($$"${chirp.email.url}") private val baseUrl: String,
+    @param:Value($$"${chirp.email.from") private val emailFrom: String
+) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun sendVerificationEmail(
+        email: String,
+        username: String,
+        userId: UserId,
+        token : String
+    ) {
+        logger.info("Sending verification email for user $userId")
+
+        val verificationUrl = UriComponentsBuilder
+            .fromUriString("$baseUrl/api/auth/verification")
+            .queryParam("token", token)
+            .build()
+            .toUriString()
+
+        val htmlContent = emailTemplateService.processTemplate(
+            templateName = "emails/account-verification",
+            variables = mapOf(
+                "username" to username,
+                "verificationUrl" to verificationUrl
+            )
+        )
+
+        sendHtmlEmail(
+            to = email,
+            subject = "Verification Email",
+            html = htmlContent,
+        )
+    }
+
+    fun sendPasswordResetEmail(
+        email: String,
+        username: String,
+        userId: UserId,
+        token : String,
+        expiresIn: Duration
+    ) {
+        logger.info("Sending password reset email for user $userId")
+
+        val resetPassordUrl = UriComponentsBuilder
+            .fromUriString("$baseUrl/api/auth/reset-password")
+            .queryParam("token", token)
+            .build()
+            .toUriString()
+
+        val htmlContent = emailTemplateService.processTemplate(
+            templateName = "emails/reset-password",
+            variables = mapOf(
+                "username" to username,
+                "resetPasswordUrl" to resetPassordUrl,
+                "expiresInMinutes" to expiresIn.toMinutes()
+            )
+        )
+
+        sendHtmlEmail(
+            to = email,
+            subject = "Reset your chirp password",
+            html = htmlContent,
+        )
+    }
+
+    private fun sendHtmlEmail(
+        to: String,
+        subject: String,
+        html: String,
+    ) {
+        val message = javaMailSender.createMimeMessage()
+        MimeMessageHelper(message, true, "UFT-8").apply {
+            setFrom(emailFrom)
+            setTo(to)
+            setSubject(subject)
+            setText(html, true)
+        }
+
+        try {
+            javaMailSender.send(message)
+        } catch (e: MailException) {
+            logger.error("Could not send email", e)
+        }
+    }
+}
